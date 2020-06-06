62ee4848523f5966b421454a53e816b9d48f9f3e
budi.hermansyah@fazzdoc.com <budi.hermansyah@fazzdoc.com>
2020-06-06 14:18:51 +0700

    tutorial 3 django

--- training/polls/urls.py
+++ training/polls/urls.py
@@ -3,6 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # /polls/
+    # ex: /polls/
     path('', views.index, name='index'),
+    path('index2', views.index2, name='index2'),
+    path('index3', views.index3, name='index3'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
